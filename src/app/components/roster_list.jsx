fea5cbffbbb1fce2ade5d1a96bb2d458a9867b59
Eugen Rochko <eugen@zeonfederated.com>
2015-09-11 18:49:52 +0200

    Improve roster sorting

--- src/app/components/roster_list.jsx
+++ src/app/components/roster_list.jsx
@@ -15,22 +15,39 @@ let RosterList = React.createClass({
     let unread = 0;
 
     let roster = this.state.items.sort(function (a, b) {
-      if (a.get('last_activity') === null && b.get('last_activity') === null) {
-        return a.get('resources').size > b.get('resources').size ? -1 : 1;
+      // 1. Sort by last_activity
+      // 2. Sort by who was last_seen online
+      // 3. Sort by who's online
+
+      let a_last   = a.get('last_activity', a.get('last_seen', null));
+      let b_last   = b.get('last_activity', b.get('last_seen', null));
+      let a_online = a.get('resources').size > 0;
+      let b_online = b.get('resources').size > 0;
+
+      if (a_last === null && b_last === null) {
+        if (a_online && !b_online) {
+          return -1;
+        } else if (b_online && !a_online) {
+          return 1;
+        } else {
+          return 0;
+        }
       }
 
-      if (a.get('last_activity') === null && b.get('last_activity') != null) {
+      if (a_last === null && b_last != null) {
         return 1;
       }
 
-      if (b.get('last_activity') === null && a.get('last_activity') != null) {
+      if (b_last === null && a_last != null) {
         return -1;
       }
 
-      if (moment(a.get('last_activity')).isBefore(b.get('last_activity'))) {
+      if (moment(a_last).isBefore(b_last)) {
         return 1;
+      } else if (moment(b_last).isBefore(a_last)) {
+        return -1;
       } else {
-        return -1;
+        return 0;
       }
     }).map(function (u) {
       unread = unread + u.get('unread');
